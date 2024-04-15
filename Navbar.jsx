67c50fb2177fe72190c997ccfbb6a90fecbab6b5
Aniket_Nirwal <76916508+Aniket-Nirwal@users.noreply.github.com>
2024-04-15 22:11:05 +0530

    Add files via upload
    Dark and Light Mode App using Reactjs context Hook

---
+++ Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useTheme } from '../theme-context'
+const Navbar = () => {
+  const {theme, toggleTheme}= useTheme()
+
+  const toggleMode = () => {
+    toggleTheme()
+  }
+  return (
+    <nav className={`navabr &{theme}`}>
+    <div>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
+    <Link to='/blog'>Blog</Link>
+    </div>
+    <div className='mode-switch'>
+    <label htmlFor="">
+      <input type="checkbox"
+      onChange={toggleMode}
+      checked={theme==="dark"} />
+       <span className="slider round"></span>
+    </label>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar
